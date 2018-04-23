0ad57063022664a16fd9dcf62a88ffdbb4813fa0
Luís Henrique <luish.faria@gmail.com>
2018-04-22 22:55:13 -0300

    fix webpack config for production

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,12 +5,7 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 // @ts-ignore
 module.exports = merge(common, {
     externals: {
-        react: {
-            commonjs: "react",
-            commonjs2: "react",
-            amd: "react",
-            root: "React"
-        }
+        "react": "React"
     },
     plugins: [
         new UglifyJSPlugin()
